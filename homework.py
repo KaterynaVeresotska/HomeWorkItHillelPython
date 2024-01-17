70e4c346594cb3b38af3b6c94287abe8a13eb810
Kateryna Veresotska <145865349+KaterynaVeresotska@users.noreply.github.com>
2024-01-17 10:48:58 +0100

    HomeWork2

---
+++ homework.py
@@ -0,0 +1,9 @@
+outside_input = int(input("add 4 digits: "))
+number_4=outside_input%10
+number_3=(outside_input//10)%10
+number_2=(outside_input//100)%10
+number_1=(outside_input//1000)%10 
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_4)
